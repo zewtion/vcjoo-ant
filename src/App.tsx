4418142891752aaecd903e1e602bc01ae68c1286
AYAN <zewtion@gmail.com>
2018-08-24 18:28:44 +0900

    -

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { Button, InputNumber } from 'antd';
+import { Button } from 'antd';
 import * as React from 'react';
 import './App.css';
+import LifeCycle from './Components/LifeCycle';
 
-import logo from './logo.svg';
 interface InterState{ num:number }
 
 
@@ -14,27 +14,14 @@ class App extends React.Component<{},InterState> {
     }
   }
 
-  public clickHandler = (i:number) =>  {
-    window.console.log("counsted: " + i);
-    this.setState({
-        num : i
-      }
-    )
-  }
-
   public render() {
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <InputNumber min={1} max={10} defaultValue={3}  onChange={this.clickHandler}/>
-        <Button type="primary" htmlType="button" onClick={callExp(5)}>Primary</Button>
-        <p className="App">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-          component by reaCt !!
-        </p>
+        <h1 className="App-title">Hello Ayan</h1>
+        <Button type="normal" htmlType="button" onClick={callExp(5)}>LifeCycle</Button>
+        <p className="App"> Click to Buttons </p>
+        <div><LifeCycle/></div>
         
       </div>
     );
---
+++ src/Components/LifeCycle.tsx
@@ -0,0 +1,18 @@
+import { Button } from 'antd/lib/radio';
+import * as React from 'react'
+
+class LifeCycle extends React.Component{
+
+
+    public render(){
+        return(
+            <>
+                <div>
+                    <Button>Hello</Button>
+                </div>
+            </>
+        );
+    }
+}
+
+export default LifeCycle;
