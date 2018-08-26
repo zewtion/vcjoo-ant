57a45978ee365e4b9fd507851012b529b3b906fe
AYAN <zewtion@gmail.com>
2018-08-26 15:39:48 +0900

    -

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,55 @@ import * as React from 'react';
 import './App.css';
 import LifeCycle from './Components/LifeCycle';
 
-interface InterState{ num:number }
+interface InterState{ 
+  num:number,
+  step:JSX.Element
+}
 
 
 class App extends React.Component<{},InterState> {
   constructor( props:any){
     super(props);
     this.state={
-      num : 0
+      num : 0,
+      step : <p> Click top menu btn </p>
+    }
+  }
+
+  public showComponents = ( flag:number ) => {
+    /* tslint:disable:no-empty */
+    if( flag === 1 ){
+      this.setState({
+        step : <LifeCycle/>
+      });
+    }else if( flag === 2 ){
+      this.setState({
+        step : <p> showing NextStep1 </p>
+      });
+    }else if( flag === 3 ){
+      this.setState({
+        step : <p> showing NextStep2 </p>
+      });
     }
   }
 
   public render() {
-
     return (
-      <div className="App">
-        <h1 className="App-title">Hello Ayan</h1>
-        <Button type="normal" htmlType="button" onClick={callExp(5)}>LifeCycle</Button>
-        <p className="App"> Click to Buttons </p>
-        <div><LifeCycle/></div>
-        
-      </div>
+      <>
+        <div className="App">
+          <h1 className="App-title">Hello Ayan</h1>
+          <Button type="Normal" size="small" htmlType="button" onClick={ this.showComponents.bind(this,1) }>LifeCycle</Button>
+          <Button type="Normal" size="small" htmlType="button" onClick={ this.showComponents.bind(this,2) }>NextStep1</Button>
+          <Button type="Normal" size="small" htmlType="button" onClick={ this.showComponents.bind(this,3) }>NextStep2</Button>
+        </div>
+        <p className="S3Line"/>
+        <div style={{textAlign:'center'}}>
+          {this.state.step} 
+        </div>
+      </>
     );
   }
 }
 
-function callExp( obj:number ){
-  window.console.log( "hello exp" + obj );
-}
 
 export default App;
--- src/Components/LifeCycle.tsx
+++ src/Components/LifeCycle.tsx
@@ -1,4 +1,3 @@
-import { Button } from 'antd/lib/radio';
 import * as React from 'react'
 
 class LifeCycle extends React.Component{
@@ -8,7 +7,7 @@ class LifeCycle extends React.Component{
         return(
             <>
                 <div>
-                    <Button>Hello</Button>
+                    LifeCycle Pannel !!
                 </div>
             </>
         );
