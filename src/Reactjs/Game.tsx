1f5f3f6a7afa1db0abdbec7c1f526aba5dde96a9
AYAN <zewtion@gmail.com>
2018-09-18 20:16:17 +0900

    complete ttt

--- src/Reactjs/Game.tsx
+++ src/Reactjs/Game.tsx
@@ -4,7 +4,8 @@ import Board from './Board';
 
 interface InterState{
   xIsNext:boolean,
-  history:any
+  history:any,
+  stepNumber:number
 }
 
 class Game extends React.Component<{},InterState>{
@@ -14,12 +15,13 @@ class Game extends React.Component<{},InterState>{
       history : [{
           squares : Array(9).fill(null)
       }],
-      xIsNext : true,
+      stepNumber : 0,
+      xIsNext : true
     };
   }
 
   public handleClick = (i:number) => {
-    const historyTemp = this.state.history;
+    const historyTemp = this.state.history.slice(0, this.state.stepNumber + 1);
     const currentTemp = historyTemp[historyTemp.length-1];
     const squaresTemp = currentTemp.squares.slice();
     if( squaresTemp[i] || this.caculateWinner(squaresTemp) ){
@@ -28,6 +30,7 @@ class Game extends React.Component<{},InterState>{
     squaresTemp[i] = this.state.xIsNext ? "X" : "O";
     this.setState({
       history: historyTemp.concat([{ squares: squaresTemp }]),
+      stepNumber : historyTemp.length,
       xIsNext: !this.state.xIsNext
     });
   }
@@ -65,16 +68,30 @@ class Game extends React.Component<{},InterState>{
     })
   }
 
+  public jumpTo = (step:number) =>{
+    this.setState({
+      stepNumber : step,
+      xIsNext : ( step%2 ) === 0
+    });
+  }
+
     public render(){
       const history = this.state.history;
-      const current = history[history.length-1];
+      const current = history[this.state.stepNumber];
       const winner = this.caculateWinner( current.squares );
-      let status;
-      if( winner ){
-        status = 'Winner is ' + winner;
-      }else{
-        status = 'Next Player is ' + (this.state.xIsNext ? 'X' : 'O');
-      }
+      const status = winner ? 'Winner is ' + winner : 'Next Player is ' + (this.state.xIsNext ? 'X' : 'O');
+
+      const moves = history.map(
+        (step:number, move:string) => {
+          const desc = move ? 'Go to move #' + move : 'Go to game start';
+          return(
+            <li key={move}>
+              <button onClick={this.jumpTo.bind(this, move)}> {desc} </button>
+            </li>
+          );
+        }
+      );
+
       return(
         <div>
           <div>
@@ -86,7 +103,7 @@ class Game extends React.Component<{},InterState>{
           </div>
           <div>
             <div>{status}</div>
-            <ol>{/** Todo */}</ol>
+            <ol>{moves}</ol>
             <div><br/>
               <Button type="Normal" size="small" htmlType="button" onClick={ this.fnReload }> Clear() </Button>
             </div>
